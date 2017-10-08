0141aa727fef7bac28c9598aceeac10812b3f041
randomchars42 <randomchars42@users.noreply.github.com>
2017-10-08 14:11:12 +0200

    update tests

--- tests/testthat/test_biosets.R
+++ tests/testthat/test_biosets.R
@@ -16,3 +16,26 @@ test_that("biosets are read correctly", {
 
   expect_true(is.tibble(sets$all))
 })
+
+test_that("biosets are cached correctly", {
+  calibrator_names <-
+    c("CAL1", "CAL2", "CAL3", "CAL4", "CAL5", "CAL6", "CAL7", "CAL8", "CAL9",
+      "CAL10")
+  calibrator_values <- exp(c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+  sets <-
+    plates_read(
+      plates = 2,
+      cal_names = calibrator_names,
+      cal_values = calibrator_values,
+      write_data = TRUE,
+      use_written_data = FALSE)
+  sets2 <-
+    plates_read(
+      plates = 2,
+      cal_names = calibrator_names,
+      cal_values = calibrator_values,
+      write_data = FALSE,
+      use_written_data = TRUE)
+
+  expect_true(is.tibble(sets2$all))
+})
